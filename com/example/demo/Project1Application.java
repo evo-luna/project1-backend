8d505deca01e9ec25413931c8959f8233545f1b4
ASananda <anandaatte@gmail.com>
2025-02-04 21:40:26 +0530

    first

---
+++ com/example/demo/Project1Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import demo.src.main.java.com.example.demo.SpringBootApplication;
+import jdk.internal.misc.InnocuousThread;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1Application.class, args);
+	}
+
+}
